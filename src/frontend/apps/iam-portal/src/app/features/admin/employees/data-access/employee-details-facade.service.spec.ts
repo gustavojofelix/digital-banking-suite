3370f9907212f3e2b70e4437d1f8825dfb4d6c38
Gustavo Felix <gustavo.felix@alvorsoftware.com>
2025-12-13 18:47:02 +0200

    test: fix tests

--- src/frontend/apps/iam-portal/src/app/features/admin/employees/data-access/employee-details-facade.service.spec.ts
+++ src/frontend/apps/iam-portal/src/app/features/admin/employees/data-access/employee-details-facade.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { EmployeeDetailsFacadeService } from './employee-details-facade.service';
+import { EmployeesApiService } from './employees-api.service';
 
 describe('EmployeeDetailsFacadeService', () => {
-  let service: EmployeeDetailsFacadeService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EmployeeDetailsFacadeService);
+    TestBed.configureTestingModule({
+      providers: [
+        EmployeeDetailsFacadeService,
+        {
+          provide: EmployeesApiService,
+          useValue: {
+            getEmployee: jest.fn(() => of(null)),
+            updateEmployee: jest.fn(() => of(void 0)),
+            activate: jest.fn(() => of(void 0)),
+            deactivate: jest.fn(() => of(void 0)),
+          },
+        },
+      ],
+    });
   });
 
   it('should be created', () => {
+    const service = TestBed.inject(EmployeeDetailsFacadeService);
     expect(service).toBeTruthy();
   });
 });
--- src/frontend/apps/iam-portal/src/app/features/admin/employees/data-access/employees-list-facade.service.spec.ts
+++ src/frontend/apps/iam-portal/src/app/features/admin/employees/data-access/employees-list-facade.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { EmployeesListFacadeService } from './employees-list-facade.service';
+import { EmployeesApiService } from './employees-api.service';
 
 describe('EmployeesListFacadeService', () => {
-  let service: EmployeesListFacadeService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EmployeesListFacadeService);
+    TestBed.configureTestingModule({
+      providers: [
+        EmployeesListFacadeService,
+        {
+          provide: EmployeesApiService,
+          useValue: {
+            listEmployees: jest.fn(() =>
+              of({
+                items: [],
+                pageNumber: 1,
+                pageSize: 10,
+                totalCount: 0,
+              })
+            ),
+          },
+        },
+      ],
+    });
   });
 
   it('should be created', () => {
+    const service = TestBed.inject(EmployeesListFacadeService);
     expect(service).toBeTruthy();
   });
 });
